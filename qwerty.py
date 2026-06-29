e0cfd36405b16558c5f94df667aebee218a8444c
qpalzmmzlapq <velesgey2286667@gmail.com>
2026-06-29 11:22:15 +0300

    Update qwerty.py

--- qwerty.py
+++ qwerty.py
@@ -1,6 +1,8 @@
+import pandas as pd 
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
 
-X, y = load_wine(return_X_y = True)
+X, y = load_wine(return_X_y = True, as_frame = True)
+print(X)
 print(y)
 
