24988f0a01f8c2b1b8664e728e1bad2972f9536e
qpalzmmzlapq <velesgey2286667@gmail.com>
2026-06-29 11:32:59 +0300

    Update qwerty.py

--- qwerty.py
+++ qwerty.py
@@ -1,8 +1,10 @@
 import pandas as pd 
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
-X, y = load_wine(return_X_y = True, as_frame = True)
+X_lw, y = load_wine(return_X_y = True)
+X = StandardScaler()
 print(X)
-print(y)
+
 
